7983ecc45192438f3667f63f214bd1f9aa087607
Feliz Salting <fpsalting@student.apc.edu.ph>
2025-01-27 14:52:30 +0800

    Added personal profile

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <h1>Food</h1>
   <food-item/>
-  <food-item2/>
-  <food-item/>
+  <food-item2/> <br>
+  <food-item/> <br>
+  <personal-profile/><br>
 </template>
 
 <script></script>
---
+++ vue-project/src/components/PersonalProfile.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="container about-me">
+        <img class="profile-image" src="https://raw.githubusercontent.com/saintcalico/webprogMI231-personal-website/main/home/fel1.jpg" alt="Feliz Salting">
+        <h2>About Me</h2>
+        <div class="content">
+            <p>
+                I'm Feliz Salting, a 2nd year BSIT-MI student in Asia Pacific College. 
+                I'm also an external relations executive director in APC-Microsoft Community. 
+                For more personal info, I have a pet dog named Jake even though she's a girl. 
+                I'm also an only child with two loving parents.
+            </p>
+            <h3>Education and Achievements | Additional Info | IT Experience | Picture Gallery</h3>
+        </div>
+    </div>
+</template>
+<script></script>
+<style></style>
