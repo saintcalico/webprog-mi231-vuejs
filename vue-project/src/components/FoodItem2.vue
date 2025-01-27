e64ee45d3142fffd3fdab733379076b5e7f0c743
Feliz Salting <fpsalting@student.apc.edu.ph>
2025-01-27 13:59:38 +0800

    updated design

--- vue-project/src/components/FoodItem2.vue
+++ vue-project/src/components/FoodItem2.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class ="mango">
         <h2>{{ name }}</h2>
         <p>{{ message }}</p>
     </div>
@@ -17,7 +17,7 @@ export default {
 </script>
 
 <style>
-#app > div {
+.mango {
         border: dashed black 1px;
         display: inline-block;
         margin: 10px;
