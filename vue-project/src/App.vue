f026945c557eab492fb467dc45a9df0cb2941bd7
Feliz Salting <fpsalting@student.apc.edu.ph>
2025-01-30 06:44:33 +0000

    Added Supabase

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <h1>Food</h1>
+  <ul>
+      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+    </ul>
+  <br>
   <food-item/>
-  <food-item2/> <br>
-  <food-item/> <br>
-  <personal-profile/><br>
+  <food-item2/> 
+  <food-item/> 
+  
 </template>
 
-<script></script>
 
+<script setup>
+  import { ref, onMounted } from 'vue'
+  import { supabase } from './lib/supabaseClient'
+
+  const countries = ref([])
+
+  async function getCountries() {
+    const { data } = await supabase.from('countries').select()
+    countries.value = data
+  }
+
+  onMounted(() => {
+    getCountries()
+  })
+  </script>
 <style>
 </style>   
