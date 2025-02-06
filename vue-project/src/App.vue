9b1b47cbd2e99f51bd6bea7ee2222e00eddc41f1
Feliz Salting <fpsalting@student.apc.edu.ph>
2025-02-07 00:25:46 +0800

    comments

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,30 +1,13 @@
 <template>
   <h1>Food</h1>
-  <ul>
-      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-    </ul>
-  <br>
-  <food-item/>
-  <food-item2/> 
+  <country/>
   <food-item/> 
+  <food-item/> 
+  <food-item/>
+  <comment-form/>
+  <comment/>
+
   
 </template>
-
-
-<script setup>
-  import { ref, onMounted } from 'vue'
-  import { supabase } from './lib/supabaseClient'
-
-  const countries = ref([])
-
-  async function getCountries() {
-    const { data } = await supabase.from('countries').select()
-    countries.value = data
-  }
-
-  onMounted(() => {
-    getCountries()
-  })
-  </script>
-<style>
-</style>   
+<script></script>
+ 
---
+++ vue-project/src/components/Country.vue
@@ -0,0 +1,48 @@
+<template>
+    <h1>Countries</h1>
+     <ul>
+      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+    </ul>
+</template>
+
+<script></script>
+
+<style>
+    #app > div{
+        border: dashed #000000 1px;
+        display:inline-block
+        margin:10px;
+        padding:10px;
+        background-color: #90EE90
+
+
+    }
+</style>
+
+
+<script setup>
+  import { ref, onMounted } from 'vue'
+  import { supabase } from './lib/supabaseClient'
+
+  const countries = ref([])
+
+  async function getCountries() {
+    const { data } = await supabase.from('countries').select()
+    countries.value = data
+  }
+
+  onMounted(() => {
+    getCountries()
+  })
+  </script>
+<style>
+    #app > div{
+        border: dashed #000000 1px;
+        display:inline-block
+        margin:10px;
+        padding:10px;
+        background-color: #90EE90
+
+
+    }
+</style>   
