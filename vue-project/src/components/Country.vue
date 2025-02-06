799ee1924b3fa757593394ea41bbd6dc6693eab8
Feliz Salting <fpsalting@student.apc.edu.ph>
2025-02-07 00:33:21 +0800

    comments

--- vue-project/src/components/Country.vue
+++ vue-project/src/components/Country.vue
@@ -1,48 +1,46 @@
 <template>
     <h1>Countries</h1>
-     <ul>
+    <ul>
       <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
     </ul>
-</template>
-
-<script></script>
-
-<style>
-    #app > div{
-        border: dashed #000000 1px;
-        display:inline-block;
-        margin:10px;
-        padding:10px;
-        background-color: #90EE90;
-
-
+  </template>
+  
+  <script></script>
+  
+  <style>
+    #app > div {
+      border: dashed black 1px;
+      display: inline-block;
+      margin: 10px;
+      padding: 10px;
+      background-color: lightyellow;
     }
-</style>
-
+  </style>
 
 <script setup>
-  import { ref, onMounted } from 'vue'
-  import { supabase } from './lib/supabaseClient'
+import { ref, onMounted } from 'vue'
+import { supabase } from '../lib/supabaseClient'
 
-  const countries = ref([])
+const countries = ref([])
 
-  async function getCountries() {
-    const { data } = await supabase.from('countries').select()
-    countries.value = data
-  }
+async function getCountries() {
+  const { data } = await supabase.from('countries').select()
+  countries.value = data
+}
 
-  onMounted(() => {
-    getCountries()
-  })
-  </script>
+onMounted(() => {
+  getCountries()
+})
+
+</script>
+
+
 <style>
-    #app > div{
-        border: dashed #000000 1px;
-        display:inline-block;
-        margin:10px;
-        padding:10px;
-        background-color: #90EE90;
-
-
-    }
-</style>   
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightyellow;
+  }
+</style>
