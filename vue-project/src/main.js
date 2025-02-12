bddebb607faacb96cd4b8f90c834f7d7e9264dfd
Feliz Salting <fpsalting@student.apc.edu.ph>
2025-02-12 23:53:17 +0800

    idk

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 
-import App from './App.vue'
-import FoodItem from './components/FoodItem.vue'
+
 import Country from './components/Country.vue'
 import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
@@ -11,7 +10,6 @@ import PersonalProfile from './components/PersonalProfile.vue'
 
 const app = createApp(App)
 
-app.component('food-item', FoodItem)
 app.component('country', Country)
 app.component('comment-form', CommentForm)
 app.component('comment', Comment)
